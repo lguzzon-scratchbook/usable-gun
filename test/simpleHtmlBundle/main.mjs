23ad6a002e9ef093fccd19b76bc624dc225f2e3a
Luca Guzzon <luca.guzzon@gmail.com>
2024-12-12 21:51:15 +0100

    feat(simpleHtmlBundle): 🚀 - Add main.mjs for Gun setup
    - Create a new file `main.mjs` in `test/simpleHtmlBundle`.
    - Import `GunEnvironment`, `defaultBrowserPlugin`, `defaultSeaPlugin`, and `radixPlugin`.
    - Initialize `GunEnvironment` with browser settings.
    - Use plugins: `defaultBrowserPlugin`, `defaultSeaPlugin`, and `radixPlugin`.
    - Set up `Gun` instance with options for storage.
    - Implement a script to read and write to the key 'data'.
    - Add real-time change event listener for 'message'.

---
+++ test/simpleHtmlBundle/main.mjs
@@ -0,0 +1,36 @@
+/* global Gun */
+import { GunEnvironment } from "../../index.js";
+import { defaultBrowserPlugin, defaultSeaPlugin } from "../../index.js"; // Equivalent to importing "gun" in a browser
+import radixPlugin from "../../lib/radix.js";
+
+const gunEnvironment = new GunEnvironment({
+	environmentHint: "browser",
+	iContributeToGun: true,
+});
+
+await gunEnvironment.usePlugins([
+	defaultBrowserPlugin,
+	defaultSeaPlugin,
+	radixPlugin,
+]);
+
+const GunOptions = {
+	file: "usable-gun--Storage",
+	// ,peers: ['https://gundb.h3r3t0.win/gun']
+};
+
+const gun = new gunEnvironment.library.Gun({
+	...GunOptions,
+});
+const sea = gunEnvironment.library.SEA;
+const Gun = gunEnvironment.library.Gun;
+(() => {
+	// Reads key 'data'.
+	const data = gun.get("data");
+	// Writes a value to the key 'data'.
+	data.put({ message: `Hello world! ${new Date().toLocaleString()}` });
+	// Listen for real-time change events.
+	data.get("message").on((message) => {
+		console.log("Message:", message);
+	});
+})();
